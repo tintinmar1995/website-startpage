a23e76a7e0fa697cf15f466301f5f4dcaf7f0d0d
tintinmar1995 <martin.masson@protonmail.com>
2020-08-06 09:36:42 +0200

    Open link in _self and autoselect search bar when loaded

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 // Optional: This just makes all the links open in a new tab
-var links = document.links;
-for (var i = 0; i < links.length; i++) {
-     links[i].target = "_blank";
-}
+//var links = document.links;
+//for (var i = 0; i < links.length; i++) {
+//     links[i].target = "_blank";
+//}
+
+
+// focus on search input
+document.getElementById("search-bar").focus();
+document.getElementById("search-bar").select();
